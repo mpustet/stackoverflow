a9dbdb9efbb5a8bea5c73f7de20ad9c287d6cb2e
mle <marcol@web.de>
2018-03-25 15:35:45 +0200

    Format

--- src/test/java/de/mle/stackoverflow/LearningTest.java
+++ src/test/java/de/mle/stackoverflow/LearningTest.java
@@ -28,48 +28,48 @@ public class LearningTest {
 				new TypeReference<Map<String, Map<String, List<String>>>>() {
 				});
 		log.info("mapOfMaps: {}", readJson);
-		
+
 		List<String> listOfFirstInnerMap = readJson.get("key1").get("value1");
 		log.info("first: {}", listOfFirstInnerMap);
 		List<String> listOfSecondInnerMap = readJson.get("key2").get("value2");
 		log.info("second: {}", listOfSecondInnerMap);
-		
-		List<String> elements = readJson.values()         // Collection<Map<String, List<String>>>
-		.stream()                                         // Stream<Map<String, List<String>>>
-		.flatMap(value -> value.values().stream())        // Stream<List<String>>
-		.flatMap(listOfStrings -> listOfStrings.stream()) // Stream<String>
-		.collect(Collectors.toList());                    // List<String>
+
+		List<String> elements = readJson.values() // Collection<Map<String, List<String>>>
+				.stream() // Stream<Map<String, List<String>>>
+				.flatMap(value -> value.values().stream()) // Stream<List<String>>
+				.flatMap(listOfStrings -> listOfStrings.stream()) // Stream<String>
+				.collect(Collectors.toList()); // List<String>
 		log.info("elements: {}", elements);
 	}
 
 	@Test
 	public void jacksonCsv() throws Exception {
-	CsvSchema schema = CsvSchema.builder()
-			.addColumn("obj")
-			.addColumn("intValue")
-			.addColumn("strValue")
-			.build()
-			.withHeader();
-	
-	ToSerialize toSerialize = new ToSerialize(Arrays.asList(new NestedObject(), new NestedObject()));
-	
-	String csv = new CsvMapper()
-			.writerFor(ToSerialize.class)
-			.with(schema)
-			.writeValueAsString(toSerialize);
-	
-	System.out.println(csv);
-		//log.info("\n{}", csv);
+		CsvSchema schema = CsvSchema.builder()
+				.addColumn("obj")
+				.addColumn("intValue")
+				.addColumn("strValue")
+				.build()
+				.withHeader();
+
+		ToSerialize toSerialize = new ToSerialize(Arrays.asList(new NestedObject(), new NestedObject()));
+
+		String csv = new CsvMapper()
+				.writerFor(ToSerialize.class)
+				.with(schema)
+				.writeValueAsString(toSerialize);
+
+		log.info("\n{}", csv);
 	}
-	
+
 	@Test
 	public void jsonIgnoreSerialization() throws Exception {
 		ObjectMapper objectMapper = new ObjectMapper();
-		
+
 		String serialized = objectMapper.writeValueAsString(new Credentials("user", "pass"));
 		assertThat(serialized).isEqualTo("{\"username\":\"user\"}");
-		
-		Credentials credentials = objectMapper.readValue("{\"username\":\"user\",\"password\":\"pass\"}", Credentials.class);
+
+		Credentials credentials = objectMapper.readValue("{\"username\":\"user\",\"password\":\"pass\"}",
+				Credentials.class);
 		assertThat(credentials.getUsername()).isEqualTo("user");
 		assertThat(credentials.getPassword()).isEqualTo("pass");
 	}
