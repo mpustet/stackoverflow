d6f473a118f40accdb902fce2346871f44f07cbd
mle <marcol@web.de>
2019-03-18 23:22:23 +0100

    Add regex matcher showcase

--- src/test/java/de/mle/stackoverflow/mockito/MockitoFeatureTest.java
+++ src/test/java/de/mle/stackoverflow/mockito/MockitoFeatureTest.java
@@ -2,19 +2,34 @@ package de.mle.stackoverflow.mockito;
 
 import org.junit.Test;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class MockitoFeatureTest {
     @Test
     public void showDoReturn() {
         Greeter greeter = mock(Greeter.class);
         doReturn("Bye!").when(greeter).greet();
+        assertThat(greeter.greet()).isEqualTo("Bye!");
+    }
+
+    @Test
+    public void showMatches() {
+        // given
+        Greeter greeter = mock(Greeter.class);
+        when(greeter.greet(matches(".*\\d+\\w.*"))).thenReturn("Matcher matched");
+
+        // when && then
+        assertThat(greeter.greet("none matching")).isNull();
+        assertThat(greeter.greet("123matching")).isEqualTo("Matcher matched");
     }
 
     class Greeter {
         String greet() {
             return "Hæ!";
+        }
+        String greet(String name) {
+            return "Hæ" + name + "!";
         }
     }
 }
