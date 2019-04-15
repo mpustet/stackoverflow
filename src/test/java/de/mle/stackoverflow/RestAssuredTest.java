6679e5eb2b0f2a5d8fc4750bcaff201afc058a1c
mle <marcol@web.de>
2019-04-15 22:19:44 +0200

    Add RESTAssured show case

---
+++ src/test/java/de/mle/stackoverflow/RestAssuredTest.java
@@ -0,0 +1,23 @@
+package de.mle.stackoverflow;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+public class RestAssuredTest {
+    @Test
+    public void getSpreadSheet() {
+        RestAssured.baseURI =
+                "https://st8.tuiprjuat.co.uk/searchpanel/departureairport/th" +
+                        "?duration=7114&multiSelect=&when=17-04-2019&flexible=true" +
+                        "&flexibleDays=3&preventCache=1555326084549";
+
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(403);
+    }
+}
