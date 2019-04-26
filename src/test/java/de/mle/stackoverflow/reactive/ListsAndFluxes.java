563b91b3ca0b289125e41f09d4057c54918bce73
Marco Leichsenring <marco.leichsenring@idealo.de>
2019-04-26 17:39:26 +0200

    Add flux and list showcase

---
+++ src/test/java/de/mle/stackoverflow/reactive/ListsAndFluxes.java
@@ -0,0 +1,27 @@
+package de.mle.stackoverflow.reactive;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class ListsAndFluxes {
+    @Test
+    public void fluxBackedList() {
+        List<Integer> a = List.of(1, 2, 3);
+        List<Integer> b = List.of(4, 5, 6);
+
+        Stream.of(a, b).parallel()
+                .flatMap(List::stream)
+                .forEach(item -> log.warn("Item in stream {}", item));
+
+        Flux.just(a, b).parallel()
+                .flatMap(list -> Flux.fromIterable(list))
+                .subscribe(item -> log.warn("Item in flux {}", item));
+    }
+}
