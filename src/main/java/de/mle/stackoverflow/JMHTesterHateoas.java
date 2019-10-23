bf3af4f03e6f7619d1189e34a530a2224cf86666
Marco Leichsenring <marco.leichsenring@idealo.de>
2019-10-23 15:55:30 +0200

    Upgrade to Spring Boot 2.2

--- src/main/java/de/mle/stackoverflow/JMHTesterHateoas.java
+++ src/main/java/de/mle/stackoverflow/JMHTesterHateoas.java
@@ -1,7 +1,7 @@
 package de.mle.stackoverflow;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
